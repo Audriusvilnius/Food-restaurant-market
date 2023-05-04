041be20cdef81550bdd7e3d3b1a4b499fe9bd4fe
audriusmacbook <audrius.ivko@gmail.com>
2023-05-04 15:19:01 +0300

    Alerts

--- resources/js/front/app.js
+++ resources/js/front/app.js
@@ -4,15 +4,15 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
-document.querySelectorAll('button')
-    .forEach(b => {
-        b.addEventListener('click', () => {
-            b.closest('.js--form').querySelectorAll('[name]')
-                .forEach(d => {
-                    console.log('d');
-                });
-        });
-    });
+// document.querySelectorAll('button')
+//     .forEach(b => {
+//         b.addEventListener('click', () => {
+//             b.closest('.js--form').querySelectorAll('[name]')
+//                 .forEach(d => {
+//                     console.log('d');
+//                 });
+//         });
+//     });
 
 
 setTimeout(function () {
@@ -21,22 +21,22 @@ setTimeout(function () {
     });
 }, 10000);
 
-mOver(function (n) {
-    let appBannersT = document.getElementsByClassName(`appBannerT${n}`);
-    let appBannersB = document.getElementsByClassName(`appBannerB${n}`);
-    for (let i = 0; i < appBannersT.length; i++) {
-        appBannersT[i].style.display = 'inline';
-        appBannersB[i].style.display = 'none';
-        appBannersT[i].style.transform = '250ms';
-    }
-});
+// mOver(function (n) {
+//     let appBannersT = document.getElementsByClassName(`appBannerT${n}`);
+//     let appBannersB = document.getElementsByClassName(`appBannerB${n}`);
+//     for (let i = 0; i < appBannersT.length; i++) {
+//         appBannersT[i].style.display = 'inline';
+//         appBannersB[i].style.display = 'none';
+//         appBannersT[i].style.transform = '250ms';
+//     }
+// });
 
-mOut(function (n) {
-    let appBannersT = document.getElementsByClassName(`appBannerT${n}`);
-    let appBannersB = document.getElementsByClassName(`appBannerB${n}`);
-    for (let i = 0; i < appBannersT.length; i++) {
-        appBannersT[i].style.display = 'none';
-        appBannersB[i].style.display = 'inline';
-        appBannersT[i].style.transform = '250ms';
-    }
-});
+// mOut(function (n) {
+//     let appBannersT = document.getElementsByClassName(`appBannerT${n}`);
+//     let appBannersB = document.getElementsByClassName(`appBannerB${n}`);
+//     for (let i = 0; i < appBannersT.length; i++) {
+//         appBannersT[i].style.display = 'none';
+//         appBannersB[i].style.display = 'inline';
+//         appBannersT[i].style.transform = '250ms';
+//     }
+// });
