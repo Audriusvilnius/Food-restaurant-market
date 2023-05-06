48c0d220699774793f537e9afcc87b69782a5770
audriusmacbook <audrius.ivko@gmail.com>
2023-05-06 23:10:34 +0300

    Home blade front

--- resources/views/front/home/home.blade.php
+++ resources/views/front/home/home.blade.php
@@ -1,14 +1,7 @@
 @extends('layouts.front')
 @section('content')
 
-{{-- <section class="py-1 text-center container">
-    <div class="col-lg-4 col-md-8 mx-auto mt-1 py-2">
-        @if(Session::has('ok'))
-        <h6 class=" alert alert-success alert-dismissible fade show border border-dark border-2" role="alert">{{Session::get('ok')}}
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></h6>
-@endif
-</div>
-</section> --}}
+
 <div class="conteiner-blue">
     <section class="py-1 text-center container shadow_new btnFront">
         <a class="list-group-item list-group-item-action" href="{{route('start')}}">
@@ -66,141 +59,115 @@
                         <h4 class="mt-3"><b><i>{{$food->title_en}}</b></i></h4>
                         @endif
                         <h3 @if($food->price<20) style="color:crimson;" @endif><b>{{__('Price') }}: <i>{{$food->price}} &euro;</b></i></h3>
-
                     </div>
                     <div class=" card-body ">
                         <div class="accordion accordion-flush" id="accordionFlushExample">
                             <div class="accordion-item">
                                 <h6 class="accordion-header " id="flush-headingOne">
                                     <button class="accordion-button collapsed rounded" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                        {{-- <a class=" list-group-item list-group-item-action " href=" {{route('list-restaurant',$food->foodReataurants_name->id)}}"> --}}
                                         <div style="font-size:17px;">
                                             <i>{{$food->foodReataurants_name->title}}</i>
-                                            {{-- <h6>{{__('Restaurant') }}: <b style="font-size:17px;"><i>
-                                                    {{$food->foodReataurants_name->title}}</b></i></h6> --}}
-                            </div>
-                            {{-- </a> --}}
-                            <div class="ms-5">
-                                {{__('Rating') }}:
-                                                                    <div id="rating-score">
-                                        <script>
-                                            for (let i = 0; i < Math.round({{ $food->rating }}); i++) {
-                                                document.write('<div class="star"></div>');
-                                            }
-                                        </script>
-                                    </div>
-                            </div>
-                            </button>
-                            </h6>
-                            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body ">
-                                    <h6>{{__('City') }}: <b><i>{{$food->foodCities_no->title}}</i></b></h6>
-                                    @if (app()->getLocale() == "lt")
-                                    <h6>{{__('Category') }}: <b><i>{{$food->foodCategory_no->title_lt}}</i></b></h6>
-                                    @else
-                                    <h6>{{__('Category') }}: <b><i>{{$food->foodCategory_no->title_en}}</i></b></h6>
-                                    @endif
-                                    <h6>{{__('Address') }}: <b><i>{{$food->foodReataurants_name->addres}}</i></b></h6>
-                                    <h6>{{__('Open') }}: <b><i>{{$food->foodReataurants_name->open}}</i></b></h6>
-                                    <h6>{{__('Close') }}: <b><i>{{$food->foodReataurants_name->close}}</i></b></h6>
-                                    @if (app()->getLocale() == "lt")
-                                    <span class="text-muted">{{$food->des_lt}}</span>
-                                    @else
-                                    <span class="text-muted">{{$food->des_en}}</span>
-                                    @endif
-
-                                </div>
-                                <form action="{{route('update-reviews')}}" method="get">
-                                    <div class="gap-3 align-items-center d-flex justify-content-center mt-3">
-                                        <input type="hidden" name="product" value="{{$food->id}}">
-                                        <div class="btn-group">
-                                            <button type="submit" class="btn btn-outline-secondary" style="width:200px;">{{__('Rating & Reviews')  }}</button>
-
                                         </div>
-                                    </div>
-                                    @csrf
-                                </form>
-                                <hr class="border border-second border-2 opacity-0">
+                                        <div class="ms-5">
+                                            {{__('Rating') }}:
+                                            <div id="rating-score">
+                                                <script>
+                                                    for (let i = 0; i < Math.round({
+                                                            {
+                                                                $food - > rating
+                                                            }
+                                                        }); i++) {
+                                                        document.write('<div class="star"></div>');
+                                                    }
 
-                                <hr class="border border-second border-2 opacity-0">
-                            </div>
-                        </div>
-                        {{-- <hr class="border border-second border-1 opacity-75"> --}}
-                        <hr class="border border-second border-2 opacity-0">
-
-                        <form action="{{route('add-basket')}}" method="post">
-                            <div class="col-md-12 gap-3 align-items-center d-flex justify-content-center">
-                                <div class="col-md-2">
-                                    {{__('Qty') }}:
-                                </div>
-                                <div class="col-md-3">
-                                    <input type="number" class="form-control" name="count" value="1" min="1">
-                                    <input type="hidden" name="id" value="{{$food->id}}">
-                                </div>
-                                <div class="col-md-1 ">
-                                    <div class="form-contro">
-                                        <button type="submit" class="btn btn-dark">
-                                            <i class="bi bi-cart-check-fill" style="font-size: 1rem"></i>
-                                        </button>
-                                    </div>
-                                    @csrf
-                                </form>
-
-                                <hr class="border border-second border-2 opacity-50">
-                                <form action="{{ route('add-basket') }}" method="post">
-                                    <div class="col-md-12 gap-3 align-items-center d-flex justify-content-center">
-                                        <div class="col-md-2">
-                                            Qty:
+                                                </script>
+                                            </div>
                                         </div>
-                                        <div class="col-md-2">
-                                            <input type="number" class="form-control" name="count" value="1"
-                                                min="1">
-                                            <input type="hidden" name="id" value="{{ $food->id }}">
-                                        </div>
-                                        <div class="col-md-1 ">
-                                            <div class="form-contro">
-                                                <button type="submit" class="btn btn-dark">
-                                                    <i class="bi bi-cart-check-fill"></i>
-                                                </button>
+                                    </button>
+                                </h6>
+                                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body ">
+                                        <h6>{{__('City') }}: <b><i>{{$food->foodCities_no->title}}</i></b></h6>
+                                        @if (app()->getLocale() == "lt")
+                                        <h6>{{__('Category') }}: <b><i>{{$food->foodCategory_no->title_lt}}</i></b></h6>
+                                        @else
+                                        <h6>{{__('Category') }}: <b><i>{{$food->foodCategory_no->title_en}}</i></b></h6>
+                                        @endif
+                                        <h6>{{__('Address') }}: <b><i>{{$food->foodReataurants_name->addres}}</i></b></h6>
+                                        <h6>{{__('Open') }}: <b><i>{{$food->foodReataurants_name->open}}</i></b></h6>
+                                        <h6>{{__('Close') }}: <b><i>{{$food->foodReataurants_name->close}}</i></b></h6>
+                                        @if (app()->getLocale() == "lt")
+                                        <span class="text-muted">{{$food->des_lt}}</span>
+                                        @else
+                                        <span class="text-muted">{{$food->des_en}}</span>
+                                        @endif
+
+                                    </div>
+                                    <form action="{{route('update-reviews')}}" method="get">
+                                        <div class="gap-3 align-items-center d-flex justify-content-center mt-3">
+                                            <input type="hidden" name="product" value="{{$food->id}}">
+                                            <div class="btn-group">
+                                                <button type="submit" class="btn btn-outline-secondary" style="width:200px;">{{__('Rating & Reviews')  }}</button>
                                             </div>
                                         </div>
                                         @csrf
-                                </form>
-                            </div>
-                            <div class=" col-md-12 d-flex">
-                                <div class="col-md-4">
+                                    </form>
+                                    <hr class="border border-second border-2 opacity-0">
                                 </div>
-
-                                @csrf
                             </div>
+                            {{-- <hr class="border border-second border-1 opacity-75"> --}}
+                            <hr class="border border-second border-2 opacity-0">
+
+                            <form action="{{route('add-basket')}}" method="post">
+                                <div class="col-md-12 gap-3 align-items-center d-flex justify-content-center">
+                                    <div class="col-md-2">
+                                        {{__('Qty') }}:
+                                    </div>
+                                    <div class="col-md-3">
+                                        <input type="number" class="form-control" name="count" value="1" min="1">
+                                        <input type="hidden" name="id" value="{{$food->id}}">
+                                    </div>
+                                    <div class="col-md-1 ">
+                                        <div class="form-contro">
+                                            <button type="submit" class="btn btn-dark">
+                                                <i class="bi bi-cart-check-fill" style="font-size: 1rem"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                @csrf
+                            </form>
+                        </div>
+                        <div class=" col-md-12 d-flex">
+                            <div class="col-md-4">
+                            </div>
+                        </div>
+                        @csrf
                         </form>
+                    </div>
+                </div>
+            </div>
+            @empty
+
+            <div class="col-xxl-12 col-xl-12 col-lg-12">
+                <div class="card shadow bg-body-tertiary rounded d-flex ">
+                    <div class="card-header justify-content-md-between align-items-center">
+                        <h1>{{__('Oops! No match found. Try again')  }}</h1>
 
                     </div>
-
+                    <div class="card-header justify-content-md-between align-items-center">
+                        <a href="{{route('start')}}" class="btn btn-secondary">{{__('BACK') }}</a>
+                    </div>
                 </div>
+                @endforelse
             </div>
-        @empty
-
-        <div class="col-xxl-12 col-xl-12 col-lg-12">
-            <div class="card shadow bg-body-tertiary rounded d-flex ">
-                <div class="card-header justify-content-md-between align-items-center">
-                    <h1>{{__('Oops! No match found. Try again')  }}</h1>
-
-                </div>
-                <div class="card-header justify-content-md-between align-items-center">
-                    <a href="{{route('start')}}" class="btn btn-secondary">{{__('BACK') }}</a>
-                </div>
-            </div>
-            @endforelse
-        </div>
-        <div class="mt-4">
-            @if ($perPageShow != 'All')
+            <div class="mt-4">
+                @if ($perPageShow != 'All')
                 {{ $foods->links() }}
-            @endif
+                @endif
+            </div>
         </div>
+        <hr class="border border-second border-0 opacity-50 m-1">
     </div>
-    <hr class="border border-second border-0 opacity-50 m-1">
-    </div>
-</div>
-<hr class="border border-second border-0 opacity-50 m-1">
-@endsection
+
+    @endsection
