9704eb890fad555b06d5f8fc563cf86ae97006ba
audriusmacbook <audrius.ivko@gmail.com>
2023-05-16 22:21:39 +0300

    User

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -44,23 +44,20 @@ class OrderController extends Controller
                         $food->title_lt = $food_id->title_lt;
                         $food->title_en = $food_id->title_en;
                         $food->price = $food_id->price;
-                        if ($food->rest_id == Auth::user()->id) {
-                            $food->data += [$key => [
-                                'rest_id' => $food->rest_id,
-                                'rest_title' => $food->rest_title,
-                                'id' => $basket->id,
-                                'title_lt' => $food->title_lt,
-                                'title_en' => $food->title_en,
-                                'qty' => $basket->count,
-                                'price' => $food->price,
-                            ]];
-                        }
+                        // if ($food->rest_id == Auth::user()->id) {
+                        $food->data += [$key => [
+                            'rest_id' => $food->rest_id,
+                            'rest_title' => $food->rest_title,
+                            'id' => $basket->id,
+                            'title_lt' => $food->title_lt,
+                            'title_en' => $food->title_en,
+                            'qty' => $basket->count,
+                            'price' => $food->price,
+                        ]];
+                        // }
                     }
                     return $food;
                 });
-            // foreach ($orders as $data) {
-            //     dump($data->data);
-            // }
             return view('back.orders.user', [
                 'orders' => $orders
             ]);
--- resources/views/back/orders/user.blade.php
+++ resources/views/back/orders/user.blade.php
@@ -6,11 +6,10 @@
             <div class="card-header">
                 <h1>{{__('All Orders')  }}</h1>
             </div>
-
             @include('alerts.alert')
-            @foreach($orders as $order)
-            {{-- @if($order->status != 3) --}}
 
+            @foreach($orders as $order)
+            @foreach ($order->data as $food)
             <div id="{{ $order['id'] }}" class="card mt-12 mt-4" style="max-width: 1wm;">
                 <div class="row g-0 shadow p-3 bg-body-tertiary rounded">
                     <div class="col-md-6">
@@ -29,7 +28,6 @@
                             <h5>{{__('Order complete')  }}</h5>
                             @endif
                             - {{$order->created_at}}
-
                         </div>
                     </div>
                     <div class="col-md-1">
@@ -42,7 +40,6 @@
                             <h6>{{__('User ID')  }}: <b><i>{{$order->user->id}}</b></i></h6>
                         </div>
                     </div>
-                    @foreach ($order->data as $food)
                     @dump($food)
                     <div class="col-md-2">
                         <div class="card-body align-content-center">
@@ -64,11 +61,9 @@
                             {{__('qty') }}: <b><i>{{$food['qty']}}</b></i>
                             {{__('Sum') }}: <b><i>{{$food['price']*$food['qty']}} &euro;</b></i></p>
                     </div>
-                    @endforeach
-
                     <div class="col-md-3">
                         <div class="card-body" style="background-color:rgba(224, 219, 219, 0.378);;border-radius:5px;">
-                            {{-- <h5>{{__('Total sum.')  }}: <b><i>{{$order->baskets->total}} &euro;</b></i></h5> --}}
+                            <h5>{{__('Total sum.')  }}: <b><i>{{$order->baskets->total}} &euro;</b></i></h5>
                         </div>
                     </div>
 
@@ -94,12 +89,10 @@
                                 <button type="submit" class="btn btn-success float-end">{{__('To ship')  }}</button>
                                 @csrf
                                 @method('post')
-
                             </form>
                             @endif
                             @if($order->status == 3) <form action="{{route('order-delete', $order)}}" method="post">
                                 <button type="submit" class="btn btn-danger float-end" @if($order->status !=3)disabled @endif>{{__('Delete') }}</button>
-
                                 @csrf
                                 @method('delete')
                             </form>
@@ -108,21 +101,9 @@
                     </div>
                 </div>
             </div>
-            {{-- @endif --}}
+            @endforeach
             @endforeach
         </div>
     </div>
 </div>
-</div>
-
 @endsection
-
-
-
-{{-- @elseif($order->status == 1)
-<h5>{{__('Order confirmed')  }}</h5>
-@elseif($order->status == 2)
-<h5>{{__('Order complete')  }}</h5>
-
-<button type="submit" class="btn btn-success m-1">{{__('Receive') }}</button>
-<button type="submit" class="btn btn-warning m-1">{{__('Confirm') }}</button> --}}
