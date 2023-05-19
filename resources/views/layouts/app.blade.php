17f4872e6b3fbcadfd4add18111aab4a1b70ab66
audriusmacbook <audrius.ivko@gmail.com>
2023-05-19 10:01:02 +0300

    App

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,41 +5,34 @@
 @inject('category', 'App\Services\CategoryService')
 @inject('order', 'App\Services\OrderService')
 
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
-    <meta charset="utf-8" data-bs-theme="dark">
-
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
-    <link href="{{asset('assets/css/owl.carousel.min.css')}}" rel="stylesheet">
-    <link href="{{asset('assets/css/owl.theme.default.min.css')}}" rel="stylesheet">
 
     <!-- Scripts -->
+    @vite(['resources/sass/back/app.scss', 'resources/js/back/app.js'])
+
     <script src="{{asset('assets/js/jquery-3.6.4.min.js')}}"></script>
     <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
     <script src="{{asset('assets/js/time.js')}}"></script>
 
-    @vite(['resources/sass/front/app.scss', 'resources/js/front/app.js'])
 
 </head>
-
-<body class="mystyle">
-    <div id="app ">
+<body class="bg-blue">
+    <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">{{--fixed-top--}}
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -190,7 +183,6 @@
             </div>
         </nav>
     </div>
-
     @include('layouts.svg')
     <main class="mystyle">
         @yield('content')
